00b6e8b2b7c735ad46895c5059ef469c32427041
Jahan Zeb Ahmed <jjahanzebb@gmail.com>
2022-09-27 01:05:35 -0700

    day 2.0

--- App.js
+++ App.js
@@ -1,20 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { Provider } from "react-redux";
+import { store } from "./store";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+// Screens //
+import HomeScreen from "./screens/HomeScreen";
+import MapScreen from "./screens/MapScreen";
+
+// Google Directions API Key: AIzaSyAANj7dQAvBvAefe8PmfvOtfNL1SaHzU8A
+
+//left at 1:37:36 in video - Google Places Autocomplete
+
+// 1) Setting up redux
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <NavigationContainer>
+        <SafeAreaProvider>
+          <Stack.Navigator>
+            <Stack.Screen
+              name="HomeScreen"
+              component={HomeScreen}
+              options={{ headerShown: false }}
+            />
+
+            <Stack.Screen
+              name="MapScreen"
+              component={MapScreen}
+              options={{ headerShown: false }}
+            />
+          </Stack.Navigator>
+        </SafeAreaProvider>
+      </NavigationContainer>
+    </Provider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ slices/navSlice.js
@@ -0,0 +1,38 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  origin: null, //point A (where are you right now)
+  destination: null, //point B (where you want to be)
+  travelTimeInformation: null, //time it takes to go from A to B
+};
+
+export const navSlice = createSlice({
+  name: "nav",
+  initialState,
+  reducer: {
+    //current state, action is when we dispatch
+    setOrigin: (state, action) => {
+      state.origin = action.payload;
+    },
+
+    setDestination: (state, action) => {
+      state.destination = action.payload;
+    },
+
+    setTravelTimeInformation: (state, action) => {
+      state.travelTimeInformation = action.payload;
+    },
+  },
+});
+
+//to use actions in other parts of code
+export const { setOrigin, setDestination, setTravelTimeInformation } =
+  navSlice.actions;
+
+//Selectors //direct return is without curly braces
+export const selectOrigin = (state) => state.nav.origin;
+export const selectDestination = (state) => state.nav.destination;
+export const selectTravelTimeInformation = (state) =>
+  state.nav.travelTimeInformation;
+
+export default navSlice.reducer;
